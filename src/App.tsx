25bb3096a683cd5c9dec7dcc11614b2a92a6e891
kennethlng <76455368+kennethfreeflow@users.noreply.github.com>
2022-05-08 14:57:23 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { SignInForm } from "@minikind/core";
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  User,
+} from "firebase/auth";
+import { auth } from "./api/firebase";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [isInitialized, setIsInitialized] = useState(false);
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [authUser, setAuthUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsSignedIn(true);
+        setAuthUser(user);
+      } else {
+        setIsSignedIn(false);
+        setAuthUser(null);
+      }
+
+      setIsInitialized(true);
+    });
+  }, []);
+
+  const handleSignInSubmit = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {}
+  };
+
+  if (!isInitialized) {
+    return <div>Loading</div>;
+  }
+
+  if (!isSignedIn) {
+    return (
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        <div>
+          <SignInForm
+            email={email}
+            password={password}
+            onEmailChange={setEmail}
+            onPasswordChange={setPassword}
+            onSubmit={handleSignInSubmit}
+          />
+        </div>
+      </div>
+      // <div className="h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      //   <div className="max-w-md w-full space-y-8">
+
+      //   </div>
+      // </div>
+    );
+  }
+
+  return <div>Hello</div>;
 }
 
 export default App;

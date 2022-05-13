2abe077d6b1756a2190606872741c4df4674a8ee
Kenneth Ng <76455368+kennethfreeflow@users.noreply.github.com>
2022-05-13 16:16:30 -0700

    Update App with display name form

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import React, { useEffect, useState } from "react";
-import { SignInForm } from "@minikind/core";
+import {
+  Box,
+  Button,
+  Container,
+  Header,
+  Input,
+  Logo,
+  SignInForm,
+  Typography,
+} from "@minikind/core";
 import {
   onAuthStateChanged,
   signInWithEmailAndPassword,
   User,
 } from "firebase/auth";
-import { auth } from "./api/firebase";
+import { auth, db } from "./api/firebase";
+import { getDoc, doc, updateDoc } from "firebase/firestore";
 
 function App() {
   const [isInitialized, setIsInitialized] = useState(false);
   const [isSignedIn, setIsSignedIn] = useState(false);
+  const [authUser, setAuthUser] = useState<User | null>(null);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [authUser, setAuthUser] = useState<User | null>(null);
+  const [displayName, setDisplayName] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -28,10 +40,39 @@ function App() {
     });
   }, []);
 
+  useEffect(() => {
+    if (!authUser) return;
+
+    const userDoc = doc(db, "users", authUser.uid);
+    getDoc(userDoc).then((snapshot) => {
+      if (!snapshot.exists()) return;
+      const data = snapshot.data();
+      setDisplayName(data["display_name"] ?? "");
+    });
+  }, [authUser]);
+
   const handleSignInSubmit = async () => {
     try {
       await signInWithEmailAndPassword(auth, email, password);
     } catch (err) {}
+  };
+
+  const handleSaveSubmit: React.FormEventHandler<HTMLFormElement> = async (
+    e
+  ) => {
+    e.preventDefault();
+    try {
+      if (!authUser || loading) {
+        return;
+      }
+      setLoading(true);
+      await updateDoc(doc(db, "users", authUser.uid), {
+        display_name: displayName,
+      });
+      setLoading(false);
+    } catch (e) {
+      console.log("Error updating profile: ", e);
+    }
   };
 
   if (!isInitialized) {
@@ -40,30 +81,48 @@ function App() {
 
   if (!isSignedIn) {
     return (
-      <div
-        style={{
-          display: "flex",
-        }}
-      >
-        <div>
-          <SignInForm
-            email={email}
-            password={password}
-            onEmailChange={setEmail}
-            onPasswordChange={setPassword}
-            onSubmit={handleSignInSubmit}
-          />
-        </div>
-      </div>
-      // <div className="h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      //   <div className="max-w-md w-full space-y-8">
-
-      //   </div>
-      // </div>
+      <Container>
+        <SignInForm
+          email={email}
+          password={password}
+          onEmailChange={setEmail}
+          onPasswordChange={setPassword}
+          onSubmit={handleSignInSubmit}
+        />
+      </Container>
     );
   }
 
-  return <div>Hello</div>;
+  return (
+    <>
+      <Header>
+        <Logo />
+      </Header>
+      <Container>
+        <Typography variant="h2">Update your profile</Typography>
+        <Box
+          as="form"
+          spaceBetweenY={2}
+          noValidate
+          onSubmit={handleSaveSubmit}
+          style={{ marginTop: 32 }}
+        >
+          <Input
+            id="display-name"
+            type="text"
+            placeholder="Display Name"
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
+            fullWidth
+            label="Display Name"
+          />
+          <Button fullWidth type="submit" disabled={loading}>
+            Save
+          </Button>
+        </Box>
+      </Container>
+    </>
+  );
 }
 
 export default App;
